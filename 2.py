ef9d0b90a5365194c228a1d1888439285bfc52c7
DW-K <pch145@naver.com>
2021-11-19 16:35:56 +0900

    try add to auto_ml

--- 2.py
+++ 2.py
@@ -6,8 +6,9 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import RobustScaler
 
+from sklearn.svm import SVC
 import xgboost as xgb
-from sklearn.ensemble import VotingClassifier, RandomForestClassifier
+from sklearn.ensemble import VotingClassifier, GradientBoostingClassifier, RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 
@@ -61,6 +62,7 @@ def encoding_column(encoder, X_scaled, X):
 
 def autoMl(X_modeling, y_modeling, scalers, encoders, models, params_dict):
     best_accuracy = {}
+    train_param_result = params_dict.copy()
     # find the best parameter by using grid search
     for scaler_key, scaler in scalers.items():
         print(f'--------scaler: {scaler_key}--------')
@@ -79,27 +81,52 @@ def autoMl(X_modeling, y_modeling, scalers, encoders, models, params_dict):
                 start_time = time.time()  # for check running time
 
                 # grid search
-                grid = GridSearchCV(estimator=model, param_grid=params_dict[model_key], cv=5)
+                grid = GridSearchCV(estimator=model, param_grid=params_dict[model_key], cv=5, verbose=2)
                 grid.fit(X_train, y_train)
                 print(f'params: {grid.best_params_}')
                 best_model = grid.best_estimator_
                 predict = best_model.predict(X_test)
                 score = accuracy_score(y_test, predict)
 
+                train_score = grid.cv_results_['mean_test_score']
+                train_params = grid.cv_results_['params']
+
+                print(f'-----------------\n{train_score}')
+                print(f'-----------------\n{train_params}')
+
+                flag = False
+                for s, ps in zip(train_score, train_params):
+                    if flag is False:
+                        pre_s = s
+                        pre_ps = ps
+                        flag = True
+                    else:
+                        for key in pre_ps.keys():
+                            if pre_ps[key] != ps[key]:
+                                if pre_s > s:
+                                    train_param_result[model_key][key] = -1
+                                elif pre_s < s:
+                                    train_param_result[model_key][key] = 1
+                                else:
+                                    train_param_result[model_key][key] = 0
+
                 target_dict = {'model': model_key, 'score': score, 'estimator': best_model, 'scaler': scaler_key,
                                'encoder': encoder_key, 'param': grid.best_params_}
 
                 if model_key not in best_accuracy.keys():
                     best_accuracy[model_key] = {'score': -1}
+                # if model_key not in best_accuracy.keys():
+                #     best_accuracy[model_key] = []
 
                 elif best_accuracy[model_key]['score'] < target_dict['score']:
                     best_accuracy[model_key] = target_dict
+                # best_accuracy[model_key].append(target_dict)
 
                 print(f'score: {score}')
                 end_time = time.time()  # for check running time
                 print(f'running time: {end_time - start_time}  cur_time: {datetime.datetime.now()}', end='\n\n')
 
-    return best_accuracy
+    return train_param_result, best_accuracy
 
 
 def validate(X, y, scalers, encoders, best_param_dict):
@@ -112,10 +139,8 @@ def validate(X, y, scalers, encoders, best_param_dict):
         best_param = param_info['param']
         model = param_info['estimator']
 
-        if 'estimators' in param_info.keys():
-            print(f"model: {model_key}\tscaler: {param_info['scaler']}\tencoder: {param_info['encoder']}\tparam: {best_param}, estimators: {param_info['estimators']}")
-        else:
-            print(f"model: {model_key}\tscaler: {param_info['scaler']}\tencoder: {param_info['encoder']}\tparam: {best_param}")
+        print(
+            f"model: {model_key}\tscaler: {param_info['scaler']}\tencoder: {param_info['encoder']}\tparam: {best_param}")
 
         X_scaled = scaler.fit_transform(X[get_numeric_col(X)])
         X_encoded = encoding_column(encoder, X_scaled, X)
@@ -136,7 +161,7 @@ def getBestParam(best_accuracy):
     for model_name, result in best_accuracy.items():
         best_param[model_name] = {}
         for k, v in result.items():
-            if k in ['estimator', 'scaler', 'encoder', 'param', 'estimators']:
+            if k in ['estimator', 'scaler', 'encoder', 'param']:
                 best_param[model_name][k] = v
 
     return best_param
@@ -156,7 +181,7 @@ def getVotingModel(models, voting_list, best_accuracy):
 
     Voting = VotingClassifier(estimators=voting_estimator_list)
 
-    return Voting, voting_estimator_list
+    return Voting
 
 
 # function for find the best accuracy given datas, params
@@ -164,36 +189,35 @@ def find_best(X, y, scalers, encoders, models, params_dict, voting_list):
     # make validation set
     X_modeling, X_val, y_modeling, y_val = train_test_split(X, y, test_size=0.2)
 
-    best_accuracy = autoMl(X_modeling, y_modeling, scalers, encoders, models, params_dict)
+    train_param, best_accuracy = autoMl(X_modeling, y_modeling, scalers, encoders, models, params_dict)
+
+    # make voting classifier's parameter
+    voting_param = {}
+    voting_param["Voting"] = {}
+    voting_param["Voting"]["voting"] = ["hard", "soft"]
+
+    Voting = getVotingModel(models, voting_list, best_accuracy)
+
+    voting_model = {"Voting": Voting}
+
+    voting_train_param, voting_accuracy = autoMl(X_modeling, y_modeling, scalers, encoders, voting_model, voting_param)
+
+    best_accuracy.update(voting_accuracy)
+
+    train_param.update(voting_train_param)
 
     best_param = getBestParam(best_accuracy)
 
-    if voting_list:
-        # make voting classifier's parameter
-        voting_param = {}
-        voting_param["Voting"] = {}
-        voting_param["Voting"]["voting"] = ["hard", "soft"]
+    # validate(X_val, y_val, scalers, encoders, best_param)
 
-        Voting, voting_estimator_list = getVotingModel(models, voting_list, best_accuracy)
-
-        voting_model = {"Voting": Voting}
-
-        voting_accuracy = autoMl(X_modeling, y_modeling, scalers, encoders, voting_model, voting_param)
-
-        best_accuracy.update(voting_accuracy)
-
-        best_param['Voting']['estimators'] = voting_estimator_list
-
-    validate(X_val, y_val, scalers, encoders, best_param)
-
-    return best_accuracy
+    return train_param, best_accuracy
 
 
 # function for set hyper parameters and run find_best
 def train():
     df = pd.read_csv(r'./dataset/afterPreprocessing.csv')
 
-    # df = df.iloc[:20, :]
+    df = df.iloc[:20, :]
 
     df_make_class = makePriceToClass(df.copy())
 
@@ -219,6 +243,7 @@ def train():
     decision_tree_gini = DecisionTreeClassifier(criterion='gini')
     decision_tree_entropy = DecisionTreeClassifier(criterion='entropy')
     knn = KNeighborsClassifier()
+    gboost = GradientBoostingClassifier()
     rf = RandomForestClassifier()
     NB = GaussianNB()
 
@@ -228,33 +253,68 @@ def train():
 
     encoders = {"one-hot encoder": oneHot_encoder, "ordinal encoder": ordinal_encoder}
 
-    models = {'xgb': XGB, 'random_forest': rf, 'knn': knn,
+    models = {'xgb': XGB, 'gradient_boosting': gboost, 'random_forest': rf, 'knn': knn,
               'decision_tree_gini': decision_tree_gini, 'decision_tree_entropy': decision_tree_entropy,
               'NaiveBayes': NB}
 
-    params_dict = {
-        'xgb': {'min_child_weight': [2 ** r for r in range(1, 6)], 'max_depth': [2 ** r for r in range(1, 6)],
-                'gamma': [r / 10 for r in range(0, 9)]},
+    lr = 1.1
+    # params_lr_dict = {
+    #     'xgb': {'min_child_weight': 2, 'max_depth': 2,
+    #             'gamma': 1.1},
+    #
+    #     'gradient_boosting': {'learning_rate': 1.1, 'n_estimators': 2,
+    #                           'max_depth': 2},
+    #
+    #     'knn': {'n_neightbors': 2},
+    #
+    #     'random_forest': {'n_estimators': 2},
+    #
+    #     "decision_tree_entropy": {"max_depth": 2,
+    #                               "min_samples_split": 2,
+    #                               "min_samples_leaf": 2},
+    #     "decision_tree_gini": {"max_depth": 1.1,
+    #                            "min_samples_split": 2,
+    #                            "min_samples_leaf": 2},
+    #
+    #     'NaiveBayes': {'var_smoothing': 10}
+    # }
 
+    params_dict_origin = {
+        'xgb': {'min_child_weight': [1, 64], 'max_depth': [2, 64],
+                'gamma': [0.1, 1]},
 
-        'knn': {'n_neighbors': [1, 3, 5, 7, 9]},
+        'gradient_boosting': {'learning_rate': [0.1, 1], 'n_estimators': [2, 512],
+                              'max_depth': [1, 16]},
 
-        'random_forest': {'n_estimators': [r for r in range(10, 101, 10)]},
+        'knn': {'n_neightbors': [1, 16]},
 
-        "decision_tree_entropy": {"max_depth": [r for r in range(3, 11, 1)],
-                                  "min_samples_split": [r / 100 for r in range(2, 50, 5)] + [2, 4, 8, 16],
-                                  "min_samples_leaf": [r for r in range(5, 10, 1)]},
-        "decision_tree_gini": {"max_depth": [r for r in range(3, 11, 1)],
-                               "min_samples_split": [r / 100 for r in range(2, 50, 5)] + [2, 4, 8, 16],
-                               "min_samples_leaf": [r for r in range(5, 10, 1)]},
+        'random_forest': {'n_estimators': [1, 64]},
 
-        'NaiveBayes': {'var_smoothing': [10 ** r for r in range(-7, -1)]}
-        }
+        "decision_tree_entropy": {"max_depth": [2, 64],
+                                  "min_samples_split": [0.1, 1],
+                                  "min_samples_leaf": [2, 16]},
 
-    voting_list = ["decision_tree_entropy", "knn", "NaiveBayes", 'random_forest']
+        "decision_tree_gini": {"max_depth": [2, 64],
+                               "min_samples_split": [0.1, 1],
+                               "min_samples_leaf": [2, 16]},
 
-    result_dict = find_best(X=X, y=y, scalers=scalers, encoders=encoders, models=models, params_dict=params_dict,
-                            voting_list=voting_list)
+        'NaiveBayes': {'var_smoothing': [10e-9, 0.1]}
+    }
+
+    voting_list = ["decision_tree_entropy", "decision_tree_gini", "NaiveBayes", 'knn', 'random_forest']
+
+    for epoch in range(1, 100):
+        train_param, result_dict = find_best(X=X, y=y, scalers=scalers, encoders=encoders, models=models,
+                                             params_dict=params_dict_origin,
+                                             voting_list=voting_list)
+
+        for model_key, params in params_dict_origin:
+            for param_key in params.keys():
+                if train_param[model_key][param_key] == 1:
+                    params_dict_origin[param_key] *= lr
+                elif train_param[model_key][param_key] == -1:
+                    params_dict_origin[param_key] /= lr
+
 
     return result_dict
 
